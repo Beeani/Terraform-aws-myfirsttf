d574f1c428f19ae5e6013d7b1eb2b82e4b775979
beeani <beeani@gmail.com>
2020-07-23 20:33:20 -0700

    Myterraform module

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-west-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-88df2eee"
+}
+
+
+
+
